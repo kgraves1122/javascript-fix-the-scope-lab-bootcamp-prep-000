8c3ca8a908d45a8b24f9772f376e09ac631a061b
kgravesjohnson@gmail.com <kgravesjohnson@gmail.com>
2018-09-10 13:10:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,16 +20,31 @@ function add2(n) {
   return parseInt(n) + two;
 }
 
+<<<<<<< HEAD
  var funkyFunction = function () {
   return  function () {
     return "FUNKY!" ;
   }
 }
+=======
+var funkyFunction = function() {
+   return function () {
+    return "FUNKY!" ;
+  };
+};
+>>>>>>> 2f696c2dcfb0daa9745c4614ef253743740abad8
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 
 
 
+<<<<<<< HEAD
 
 var theFunk = "FUNKY!";
+=======
+var theFunk = funkyFunction(function())
+
+
+
+>>>>>>> 2f696c2dcfb0daa9745c4614ef253743740abad8
